ce53dbb4fd992bbaade06e9bbb334be6199d7802
ChechuM <ceciliammoroni@gmail.com>
2023-07-07 14:45:15 +1000

    changed versions
    Cambié las versiones de react y react-router-dom para poder usar los hooks (como useNavigate)
    Cambios en index.js y App.js para poder adaptarlos
    Creé un botón para entrar al Home desde el Landing

--- src/components/views/Landing/landing.jsx
+++ src/components/views/Landing/landing.jsx
@@ -1,8 +1,18 @@
-function Landing (){
+import './landing.module.css';
+import { React } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Landing() {
+
+    const navigate = useNavigate();
+    const getStarted = () => {
+        navigate('/home');
+    }
+
     return (
         <div>
-            <h1>Soy el landing</h1>
+            <h1>Soy el Landing</h1>
+            <button className="getStarted" onClick={() => { getStarted() }}>Get Started! </button>
         </div>
     )
 }
-export default Landing;
