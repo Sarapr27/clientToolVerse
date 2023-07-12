f0962ad4af5fe112b47611648ab2884dc76db7cd
ChechuM <ceciliammoroni@gmail.com>
2023-07-12 15:20:30 +1000

    Puliendo el Home
    Tendido de puentes para conectar el front con el back
    Pequenias modificaciones en redux

--- src/components/views/Home/home.jsx
+++ src/components/views/Home/home.jsx
@@ -3,8 +3,20 @@ import carrImg from '../img/carruselTry.png';
 import saw from '../img/chainsaw.png';
 import pruner from '../img/pruners.png';
 import trimmer from '../img/hedgetrimmer.png';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from '../../../redux/actions'
 
 function Home() {
+    // de momento no estamos conectados -> se dejan los puertos tendidos para cuando estemos listos
+    const dispatch = useDispatch();
+    const toolsShown = useSelector(state => state.toolsShown)
+
+    useEffect(() => {
+        dispatch(actions.getTools());
+    }, [dispatch])
+
+
     return (
         <div className='homeDiv'>
             <div className="carrusel">
@@ -12,12 +24,40 @@ function Home() {
                 <h3 className='enunciadoCarrusel'>Espacio designado para el Carrusel</h3>
             </div>
             <div className="highlights">
-                Estos son los productos destacados -- queremos productos detacados? los ponemos arriba en el carrusel?
+                <h3>Estos son los productos destacados -- queremos productos detacados? los ponemos arriba en el carrusel? </h3>
                 <hr />
-                También podría ir aquí un paginado
+                <h3>También podría ir aquí un paginado </h3>
                 <hr />
-                Esto está hardcodeado a la espera de la Api
+                Esto está hardcodeado a la espera de la creación de las Cards y la Api
             </div>
+
+            {/* La información que viene desde la api va a ser:
+             product {
+                id : INTEGER
+                brand : STRING,
+                name : STRING,
+                model : STRING,
+                detail : STRING,
+                price : FLOAT,
+                image : STRING
+             }
+
+
+             Entonces deberíamos hacer un map de la info que viene del store ->
+                toolsShown.map((p, i) => {
+                    return <Card
+                        id={p.id}
+                        brand={p.brand}
+                        name={p.name}
+                        model={p.model}
+                        detail={p.detal}
+                        price={p.price}
+                        image={p.image}
+                        />
+                })
+
+            */}
+
             <div className="cards">
                 <div className="upperDiv">
                     <div className="name">
@@ -61,6 +101,7 @@ function Home() {
                     </div>
                 </div>
             </div>
+            {/* aquí termina el div.cards */}
         </div>
     )
 }
