7510cef30adc69badc22830d078084c23fc8c7c6
jopez1 <jopez1@hotmail.com>
2023-07-12 12:05:29 -0500

    Cambios en  el controller

--- src/components/views/Detail/detail.jsx
+++ src/components/views/Detail/detail.jsx
@@ -1,36 +1,37 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import {useParams} from "react-router-dom"
+//import styles from "./Detail.css"
 
+const Detail = () => {
+  const [productData, setProductData] = useState(null);
+  const {id} = useParams
 
+  useEffect(() => {
+    const fetchProduc = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/products/2`);
 
-const Detail = (props) => {
+        setProductData(response.data);
+      } catch (error) {
+        console.log("Error al obtener los datos del producto:", error);
+      }
+    };
 
-    const {id, name, model, brand, price, image} = props;
-    const [productData, setProductData] = useState(null);
+    fetchProduc();
+  }, [id]);
 
-    useEffect(() => {
-        axios.get(`http://localhost:3001/products/1`)
-        .then(response => {
-            setProductData(response.data);
-        })
-        .catch(error => {
-            console.log("Error al obtener los datos del producto:", error);
-        })
-    }, [id]);
-
-  if(!productData)
-  return <div>Esperando carga del producto...</div>
-    return (
-        <div>
-            <img src={productData.image} alt={name}/>
-            <h1>{productData.name}</h1>
-            <h2>Modelo: {productData.model}</h2>
-            <h3>Marca: {productData.brand}</h3>
-            <h4>Precio {productData.price}</h4>
-            <button>Comprar</button>
-            
-        </div>
-    );
-}
+  if (!productData) return <div>Esperando carga del producto...</div>;
+  return (
+    <div>
+      <img src={productData.image} alt={productData.image} />
+      <h1>{productData.name}</h1>
+      <p>Modelo: {productData.model}</p>
+      <h3>Marca: {productData.brand}</h3>
+      <h4>Precio ${productData.price}</h4>
+      <button>Add Cart</button>
+    </div>
+  );
+};
 
 export default Detail;
