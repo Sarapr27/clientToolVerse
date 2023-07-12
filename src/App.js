e8df2637a04812de1e610d49769d71f87714eb27
jopez1 <jopez1@hotmail.com>
2023-07-12 16:44:21 -0500

    Se crea componente ProductCard

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from "./components/views/Home/home";
 import Landing from "./components/views/Landing/landing";
 import NavBar from "./components/views/NavBar/navBar";
 import Cart from "./components/views/Cart/cart";
+import ProductCard from "./components/views/ProductCard/ProductCard";
 
 function App() {
   const location = useLocation();
@@ -23,6 +24,7 @@ function App() {
         <Route path="/detail" element={<Detail />} />
         <Route path="/form" element={<Form />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/tools" element={<ProductCard/>} />
       </Routes>
     </div>
   );
