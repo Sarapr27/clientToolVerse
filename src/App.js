9a068f835a705899be5678de769b6a792957b282
ngpalma <nicolasgerardopalma@gmail.com>
2023-07-13 18:36:01 -0300

    Error404

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Landing from "./components/views/Landing/landing";
 import NavBar from "./components/views/NavBar/navBar";
 import Cart from "./components/views/Cart/cart";
 import ProductCard from "./components/views/ProductCard/ProductCard";
+import Error404 from "./components/Error404/Error404";
 
 function App() {
   const location = useLocation();
@@ -25,6 +26,7 @@ function App() {
         <Route path="/form" element={<Form />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/tools" element={<ProductCard/>} />
+        <Route path="/error" element={<Error404/>}/>
       </Routes>
     </div>
   );
