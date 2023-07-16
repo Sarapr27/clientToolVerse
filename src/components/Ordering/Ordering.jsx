ca2eacdb6d78083a345e3858b798dd9401faf1d4
ngpalma <nicolasgerardopalma@gmail.com>
2023-07-16 18:22:33 -0300

    OrderingStyles

--- src/components/Ordering/Ordering.jsx
+++ src/components/Ordering/Ordering.jsx
@@ -1,49 +1,49 @@
-import style from './Ordering.module.css'
-import {orderByName, orderByPrice} from '../../redux/actions'
-import { useDispatch } from 'react-redux';
+import style from "./Ordering.module.css";
+import { orderByName, orderByPrice } from "../../redux/actions";
+import { useDispatch } from "react-redux";
 
 const Ordering = () => {
   const dispatch = useDispatch();
 
-    return (
-        <div className={style.orderingContainer}>
-            <div className={style.ordenAlpha}>
-          <select
-            name="orderByName"
-            onChange={(e) => {
-              dispatch(orderByName(e.target.value));
-            }}
-          >
-            <option value="" key="first" hidden>
-              Orden alfabético
+  return (
+    <div className={style.orderingContainer}>
+      <div className={style.alphabeticalOrder}>
+        <select
+          name="orderByName"
+          onChange={(e) => {
+            dispatch(orderByName(e.target.value));
+          }}
+        >
+          <option value="" key="first" hidden>
+            Orden alfabético
+          </option>
+          {["A-Z", "Z-A"].map((e, i) => (
+            <option value={e} key={i}>
+              {e}
             </option>
-            {["A-Z", "Z-A"].map((e, i) => (
-              <option value={e} key={i}>
-                {e}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className={style.ordenPop}>
-          <select
-            name="orderByPrice"
-            onChange={(e) => {
-              dispatch(orderByPrice(e.target.value));
-            }}
-          >
-            <option value="" key="first" hidden>
-              Orden por precio
-            </option>
+          ))}
+        </select>
+      </div>
+      <div className={style.orderByPrice}>
+        <select
+          name="orderByPrice"
+          onChange={(e) => {
+            dispatch(orderByPrice(e.target.value));
+          }}
+        >
+          <option value="" key="first" hidden>
+            Orden por precio
+          </option>
 
-            {["Mayor precio", "Menor precio"].map((e, i) => (
-              <option value={e} key={i}>
-                {e}
-              </option>
-            ))}
-          </select>
-        </div>
-        </div>
-    )
-}
+          {["Mayor precio", "Menor precio"].map((e, i) => (
+            <option value={e} key={i}>
+              {e}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
 
 export default Ordering;
