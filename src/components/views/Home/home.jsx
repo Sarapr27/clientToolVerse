56f0ae2b49cfa7f1087b7e7cfdc3c6ca4515a8de
Sara Pinzon <sarapr.2702@gmail.com>
2023-07-17 16:02:20 -0500

    Fix en el form

--- src/components/views/Home/home.jsx
+++ src/components/views/Home/home.jsx
@@ -11,6 +11,7 @@ function Home() {
 
     return (
         <div className={style.homeDiv}>
+            <br></br>
             <Carousel showThumbs={false} autoPlay={true} interval={3000}>
                 <img src="https://belltec.com.co/modules/angarslider/views/img/images/79f2d78b09ec9efa0cb71b14a6136ed979781ce7_bannerofertasJULIO%202023%20BELLTEC.jpg" alt="img" />
                 <img src="https://belltec.com.co/modules/angarslider/views/img/images/1c2616dc4c86eccdcd219d50eeb50fb9f68ce4ae_bannerinalambricos%202022.jpg" alt="img" />
