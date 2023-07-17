511444e91a46555e8dda8a605dc4b7e7c474f461
jopez1 <jopez1@hotmail.com>
2023-07-17 18:17:29 -0500

    Se deja de utilizar funcion convertirNumero en componentes involucrados

--- src/components/views/Detail/detail.jsx
+++ src/components/views/Detail/detail.jsx
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import style from "./Detail.module.css";
 import { addToCart, getToolById } from "../../../redux/actions";
-import { convertirNumero } from "../../utils";
 
 const Detail = () => {
   const products = useSelector((state) => state.toolsDetail);
@@ -31,7 +30,7 @@ const Detail = () => {
         <h3>Marca: {products.brand}</h3>
 
         <p>Modelo: {products.model}</p>
-        <h4>Precio ${convertirNumero(products.price)}</h4>
+        <h4>Precio ${products.price}</h4>
        <br></br>
         <button className={style.addToCart} onClick={() => dispatch(addToCart(products))}><b>Add To Cart</b></button>
       </div>
