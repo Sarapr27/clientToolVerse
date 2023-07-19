9e7f9a1986df0f965ed752be30b062d5b8333d32
Sara Pinzon <sarapr.2702@gmail.com>
2023-07-19 12:08:42 -0500

    Fix en footer

--- src/components/views/Footer/Footer.jsx
+++ src/components/views/Footer/Footer.jsx
@@ -1,37 +1,35 @@
 import React from 'react';
-import logo from "../NavBar/logoTry.png"
+import logo from "./ToolVe.png"
 import styles from "./Footer.module.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
-const  Footer = () => {
-   
+const Footer = () => {
+
     return (
-        
+
         <footer className={styles.footer}>
-        <section>
-            <div className={styles['logo-container']}>
-                <Link to="/home">
-                <img src={logo} alt="logo"/>
-                </Link> 
-            </div>
-            <div className={styles['links']}>
-                <Link to="/about"><p>Nuestro Equipo</p></Link>
-                
-                <div className={styles['contact-us']}>
-                <Link to="contactus">Contactanos</Link>
+            <section>
+                <div className={styles['logo-container']}>
+                    <Link to="/home">
+                        <img src={logo} alt="logo" />
+                    </Link>
                 </div>
-                
-            </div>
-        </section>
+                <div className={styles['links']}>
+                    <a href="/about">Nuestro Equipo</a>
+                    <div className={styles['contact-us']}>
+                        <a href="/contactus">Contáctanos</a>
+                    </div>
+                </div>
+            </section>
             <div>
-            
+
             </div>
-            
+
             <p>Toolverse© 2023 Todos los derechos reservados.</p>
         </footer>
 
-       
+
     )
 };
 
