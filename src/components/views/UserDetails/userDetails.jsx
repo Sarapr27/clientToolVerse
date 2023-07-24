dfeb88af2a719e728d753b73db15eab722f9a953
ChechuM <ceciliammoroni@gmail.com>
2023-07-24 15:09:36 +1000

    Para presentar
    Detalles finales para presentar maniana

--- src/components/views/UserDetails/userDetails.jsx
+++ src/components/views/UserDetails/userDetails.jsx
@@ -4,19 +4,23 @@ import { useNavigate } from 'react-router-dom';
 import React from "react";
 import CartForm from '../CartForm/cartForm';
 
+
 export default function UserDetails() {
     // obtiene la información si el usuario ha iniciado sesión
     // si el usuario no ha iniciado sesión, permite que lo haga
     // si el usuario es nuevo permite que coloque sus datos y pueda crear un usuario nuevo
 
-    const actualUser = useSelector(state => state.actualUser)
+    //actualUser
+
+
+    const actualUser = useSelector(state => state.login)
     const navigate = useNavigate()
 
     return (
         <div className={style.overallUser}>
             <div className={style.questions}>
                 {
-                    (!actualUser.hasOwnProperty('firstName')) ? <div className={style.noUser}>
+                    (!actualUser.hasOwnProperty('email')) ? <div className={style.noUser}>
                         <div className={style.msgLogIn}>
                             <div className={style.mje}>
                                 Parece que no has iniciado sesión.
