b63dbd4b21b27220d502fe1b96f4c1a776349341
JuanZu77 <juanvzubiri77@gmail.com>
2023-07-28 20:50:20 -0300

    conflictos con actions y redux

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -25,9 +25,9 @@ import {
   REGISTER_STOCK_EXIT_SUCCESS,
   UPDATE_TOOL_STOCK,
   ACTUAL_USER,
-  ADD_REVIEW,
-  UPDATE_REVIEW_COMMENTS,
-  DELETE_REVIEW
+  // ADD_REVIEW,
+  // UPDATE_REVIEW_COMMENTS,
+  // DELETE_REVIEW
 } from "./type";
 
 export const getToolsByName = (tool) => {
@@ -282,17 +282,17 @@ export const registerStockExit = (toolId, quantity) => async (dispatch) => {
 };
 
 //Actions Reviews
-export const addReview = (review) => ({
-  type: ADD_REVIEW,
-  payload: review,
-});
+// export const addReview = (review) => ({
+//   type: ADD_REVIEW,
+//   payload: review,
+// });
 
-export const updateReviewComments = (id, comments) => ({
-  type: UPDATE_REVIEW_COMMENTS,
-  payload: { id, comments },
-});
+// export const updateReviewComments = (id, comments) => ({
+//   type: UPDATE_REVIEW_COMMENTS,
+//   payload: { id, comments },
+// });
 
-export const deleteReview = (id) => ({
-  type: DELETE_REVIEW,
-  payload: id,
-});
+// export const deleteReview = (id) => ({
+//   type: DELETE_REVIEW,
+//   payload: id,
+// });
